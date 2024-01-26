0b6ded3a519db82def852fdb4e1201579080fd0a
maxming2333 <maxming2333@gmail.com>
2024-01-26 14:56:56 +0800

    fix: 修复循环调用问题

--- packages/eslint-plugin-tsvue/lib/processor.js
+++ packages/eslint-plugin-tsvue/lib/processor.js
@@ -3,6 +3,11 @@ const vueEslintPluginProcessor = require('eslint-plugin-vue/lib/processor');
 
 module.exports = Object.assign({}, vueEslintPluginProcessor, {
   preprocess(text, filename) {
+    // 如果已经被处理了，就不在分析
+    if (/\/\.__lint_lang_[^/]+\//i.test(filename)) {
+      return vueEslintPluginProcessor.preprocess(text);
+    }
+
     const parseResult = parser.parseComponent(text);
     const { script } = parseResult;
     const lang = script ? (script.lang || 'js') : 'js';
