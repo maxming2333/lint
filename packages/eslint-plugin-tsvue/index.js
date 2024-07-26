edac9d3a314d7c1b8f55606ab303e120cdd770fa
maxming2333 <maxming2333@gmail.com>
2024-07-26 19:52:53 +0800

    feat: 低版本兼容，改回 path 分流

--- packages/eslint-plugin-tsvue/index.js
+++ packages/eslint-plugin-tsvue/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   processors: {
+    vue: require('./lib/processor'),
     '.vue': require('./lib/processor'),
   },
 };
--- packages/eslint-plugin-tsvue/lib/processor.js
+++ packages/eslint-plugin-tsvue/lib/processor.js
@@ -1,21 +1,25 @@
 const parser = require('vue-sfc-parser');
-const vueEslintPlugin = require('eslint-plugin-vue');
+const vueEslintPluginProcessor = require('eslint-plugin-vue/lib/processor');
 
-const processor = vueEslintPlugin.processors[ '.vue' ] || vueEslintPlugin.processors.vue;
-
-module.exports = Object.assign({}, processor, {
+module.exports = Object.assign({}, vueEslintPluginProcessor, {
   preprocess(text, filename) {
+    // 如果已经被处理了，就不在分析
+    if (/\/\.__lint_lang_[^/]+\//i.test(filename)) {
+      return vueEslintPluginProcessor.preprocess(text);
+    }
+
     const parseResult = parser.parseComponent(text);
     const { script } = parseResult;
     const lang = script ? (script.lang || 'js') : 'js';
     if (/js/i.test(lang)) {
-      return processor.preprocess(text, filename);
+      return vueEslintPluginProcessor.preprocess(text);
     }
 
+    const name = filename.replace(/\.vue$/, `/.__lint_lang_${lang}/0_.vue`);
     return [
       {
         text,
-        filename: `0.${lang}vue`,
+        filename: name,
       },
     ];
   },
